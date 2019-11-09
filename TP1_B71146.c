5b31d36913561dd6a506fb6f0e1612be2a263fca
GabrielBogantes23 <56243354+GabrielBogantes23@users.noreply.github.com>
2019-11-09 16:08:09 -0600

    Add files via upload

--- TP1_B71146.c
+++ TP1_B71146.c
@@ -1,31 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void convert();
+void convertBinToTxt();
+void convertTxtToBin();
 void solve();
 void validate();
-void leerDatos();
+void leerDatosArchivo();
+
+int filas = 0;
+int columnas = 0;
+int cantidadEntradas = 0;
+int cantidadSalidas = 0;
+int posicionFilas = 0;
+int posicionColumnas = 0;
+int direccion = 0;
+
+struct Posicion{
+  int posiciones[2];
+  char direccion;
+};
 
 int main(int argc, char* argv[]){
-  if(argc <= 5){
+  if(argc <= 5 || argc >= 2){
     if(argv[1] == "convert"){
-      convertir();
-    }else if(argv[2] == "solve"){
+      if(argv[2] == "ib"){
+        convertBinToTxt();
+      }else if(argv[2] == "it"){
+        convertTxtToBin();
+      }
+    }else if(argv[1] == "solve"){
+      if(argc == 3)
       solve();
-    }else if(argv[3] == "validate"){
+    }else if(argv[1] == "validate"){
       validate();
     }
-    int filas = 0;
-    int columnas = 0;
-    int cantidadEntradas = 0;
-    int cantidadSalidas = 0;
-    leerDatos();
+    leerDatosArchivo();
   }else{
-    printf("Cantidad de eventos invalidos");
+    printf("Cantidad de argumentos invalidos, ingrese la cantidad correcta.");
     return -1;
   }
 }
 
-void leerDatos(){
+void convertBinToTxt(){
+  printf("Llamado Bin to Txt");
+}
+
+void convertTxtToBin(){
+  printf("Llamado Txt to Bin");
+}
+
+void leerDatosArchivo(){
   scanf("%d %d %d %d", &filas, &columnas, &cantidadEntradas, &cantidadSalidas);
+  while(scanf("%d %d %c", &posicionFilas, &posicionColumnas, &direccion) != EOF){
+
+  }
 }
